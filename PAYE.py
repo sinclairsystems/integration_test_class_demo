4e85f072640507a6b43eca18134bbf60d59e0c34
csinclair <chris@sinclairsystems.com>
2022-08-09 15:45:29 +1200

    fyi

---
+++ PAYE.py
@@ -0,0 +1,57 @@
+import calculation
+
+
+def lessThan14000(gross_income):
+    if gross_income < 0 or gross_income > 14000:
+        return None
+    taxrate = 10.5
+    tax = calculation.multiply(gross_income, taxrate/100)
+    return tax
+
+def lessThan48000(gross_income):
+    if gross_income <= 14000 or gross_income > 48000:
+        return None
+    taxrate = 17.5
+    tax_range = calculation.subtract(gross_income, 14000)
+    tax = calculation.multiply(tax_range, taxrate/100)
+    tax = tax + lessThan14000(14000)
+    return tax
+
+def lessThan70000(gross_income):
+    if gross_income <= 48000 or gross_income > 70000:
+        return None
+    taxrate = 30
+    tax_range = calculation.subtract(gross_income, 48000)
+    tax = calculation.multiply(tax_range, taxrate / 100)
+    tax = tax + lessThan48000(48000)
+    return tax
+
+def lessThan180000(gross_income):
+    if gross_income <= 70000 or gross_income > 180000:
+        return None
+    taxrate = 33
+    tax_range = calculation.subtract(gross_income, 70000)
+    tax = calculation.multiply(tax_range, taxrate / 100)
+    tax = tax + lessThan70000(70000)
+    return tax
+
+def overThan180000(gross_income):
+    if gross_income <= 180000:
+        return None
+    taxrate = 39
+    tax_range = calculation.subtract(gross_income, 180000)
+    tax = calculation.multiply(tax_range, taxrate / 100)
+    tax = tax + lessThan180000(180000)
+    return tax
+
+def totalTax(gross_income):
+    if gross_income > 180000:
+        return overThan180000(gross_income)
+    elif gross_income > 70000: # 70000 - 180000
+        return lessThan180000(gross_income)
+    elif gross_income > 48000: # 48000 - 70000
+        return lessThan70000(gross_income)
+    elif gross_income > 14000: # 14000 - 48000
+        return lessThan48000(gross_income)
+    else: # up to 14000
+        return lessThan14000(gross_income)
---
+++ TestPaye.py
@@ -0,0 +1,60 @@
+import unittest
+import PAYE
+
+
+class TestCalculator(unittest.TestCase):
+    def test_lessThan14000(self):
+        tax = PAYE.lessThan14000(0)
+        self.assertEqual(0, tax)
+        tax = PAYE.lessThan14000(1)
+        self.assertEqual(0.105, tax)
+        tax = PAYE.lessThan14000(14000)
+        self.assertEqual(1470, tax)
+        tax = PAYE.lessThan14000(14001) #bad test case
+        self.assertIs(None, tax)
+
+    def test_lessThan48000(self):
+        tax = PAYE.lessThan48000(14000)
+        self.assertIs(None, tax)
+        tax = PAYE.lessThan48000(14001)
+        self.assertEqual(1470.175, tax)
+        tax = PAYE.lessThan48000(48000)
+        self.assertEqual(7420, tax)
+        tax = PAYE.lessThan48000(48001)
+        self.assertIs(None, tax)
+
+    def test_lessThan70000(self):
+        tax = PAYE.lessThan70000(48000)
+        self.assertIs(None, tax)
+        tax = PAYE.lessThan70000(48001)
+        self.assertEqual(7420.3, tax)
+        tax = PAYE.lessThan70000(70000)
+        self.assertEqual(14020, tax)
+        tax = PAYE.lessThan70000(70001)
+        self.assertIs(None, tax)
+
+    def test_lessThan180000(self):
+        tax = PAYE.lessThan180000(70000)
+        self.assertIs(None, tax)
+        tax = PAYE.lessThan180000(70001)
+        self.assertEqual(14020.33, tax)
+        tax = PAYE.lessThan180000(180000)
+        self.assertEqual(50320, tax)
+        tax = PAYE.lessThan180000(180001)
+        self.assertIs(None, tax)
+
+    def test_over180000(self):
+        tax = PAYE.overThan180000(180000)
+        self.assertIs(None, tax)
+        tax = PAYE.overThan180000(180001)
+        self.assertEqual(50320.39, tax)
+
+    def total_tax(self):
+        tax = PAYE.totalTax(180000)
+        self.assertIs(None, tax)
+        tax = PAYE.overThan180000(180001)
+        self.assertEqual(50320.39, tax)
+
+
+if __name__ == '__main__':
+    unittest.main()
